9d045015c27ee4f921e60d283477e922442bb86e
KAWASAKI Yasukazu <kawasaki@dev.kawa1128.jp>
2017-03-05 17:40:49 +0900

    Changeは保存しないようにする

--- www/admin/webApp/models/maindb.py
+++ www/admin/webApp/models/maindb.py
@@ -242,6 +242,8 @@ def save_ranking_pos(tm, ranking):
     cur = con.cursor()
 
     for p in ranking:
+      if p == 'change':
+        continue
       sql = '''
       INSERT INTO
         display_rank_{} (date, rank, last_rank, keyword)
